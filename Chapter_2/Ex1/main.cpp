df69ffb158361cc663121c0b45e923d5af9f7596
Unknown <kamillo1000@gmail.com>
2017-06-15 15:35:33 +0200

    Commit chapter 2 exercise 1.

---
+++ Chapter_2/Ex1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+///Printing first, last name and address of author.
+int main() {
+    using std::cout;
+    using std::endl;
+
+    cout << "Kamil Malinowski" << endl;
+    cout << "ul. Gen. ####### ##/#" << endl;
+    cout << "##-###, Wrocław" << endl;
+}

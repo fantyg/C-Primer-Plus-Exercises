496c016cdcb3a2c4eb61e1e62d70f8ae9b0e3214
kamillo1000 <kamillo1000@gmail.com>
2017-06-30 11:12:31 +0200

    Chapter 6 exercise 6
    - Names can have more then one word
    - writing none, when there is no donators in some cathegory.

--- Chapter_6/Ex06/main.cpp
+++ Chapter_6/Ex06/main.cpp
@@ -12,30 +12,43 @@ int main() {
     int donatorCount;
     std::cout << "Hi, how many donators do you want to input: ";
     std::cin >> donatorCount;
+    std::cin.get();
     checkCin();
     Donator *donators = new Donator[donatorCount];
     for (int i = 0; i < donatorCount; ++i) {
         std::cout << "#" << i + 1 << " donator:" << std::endl
                   << "\tInput last name: ";
-        std::cin >> donators[i].lastName;
+        getline(std::cin, donators[i].lastName);
         checkCin();
         std::cout << "\tInput high of donate: ";
         std::cin >> donators[i].donate;
+        std::cin.get();
         checkCin();
     }
 
+    bool empty = true;
     std::cout << "Our Great Donators: " << std::endl;
     for (int i = 0; i < donatorCount; ++i) {
         if (donators[i].donate >= greatnessLimit) {
             std::cout << "\t" << donators[i].lastName << std::endl;
+            empty = false;
         }
     }
 
+    if (empty) {
+        std::cout << "\tnone" << std::endl;
+    }
+    empty = true;
     std::cout << "Our Donators: " << std::endl;
     for (int i = 0; i < donatorCount; ++i) {
         if (donators[i].donate < greatnessLimit) {
             std::cout << "\t" << donators[i].lastName << std::endl;
+            empty = false;
         }
+    }
+
+    if (empty) {
+        std::cout << "\tnone" << std::endl;
     }
     return 0;
 }

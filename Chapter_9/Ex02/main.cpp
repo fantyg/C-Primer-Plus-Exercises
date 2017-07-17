2bcf0bf917a99395f3570d8821b6004f073d3380
kamillo1000 <kamillo1000@gmail.com>
2017-07-17 14:20:04 +0200

    Chapter 9 exercise 2

---
+++ Chapter_9/Ex02/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void strcount(const std::string &str);
+
+int main() {
+    std::string input;
+    char next;
+
+    std::cout << "Input row:\n";
+    getline(std::cin, input);
+    while (input.compare("")) {
+        strcount(input);
+        std::cout << "Input next row (empty row ends input):\n";
+        getline(std::cin, input);
+    }
+
+    std::cout << "End\n";
+    return 0;
+}
+
+void strcount(const std::string &str) {
+    static int total = 0;
+
+    std::cout << "\"" << str << "\" contains " << str.size() << " signs.\n";
+    total += str.size();
+    std::cout << "Altogether " << total << " signs.\n";
+}
